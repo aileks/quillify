4edc7ca8eed0ae9c37bde55fca005b272e0494ce
Aaliyah Harper <aeverly14@pm.me>
2025-07-15 11:54:52 -0400

    add constraints to books

--- db/migrate/20250715154852_create_books.rb
+++ db/migrate/20250715154852_create_books.rb
@@ -1,12 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[8.0]
   def change
     create_table :books, id: :uuid do |t|
-      t.string :title
-      t.string :author
+      t.string :title, null: false
+      t.string :author, null: false
       t.integer :publication_year
       t.integer :pages
-      t.string :genre
-      t.boolean :read
+      t.string :genre, null: false
+      t.boolean :read, null: false, default: false
       t.references :user, null: false, foreign_key: true, type: :uuid
 
       t.timestamps

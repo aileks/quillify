079bc9757b01378f8af82dc49d9efaab297f068d
Aaliyah Harper <aaliyah@aileks.dev>
2025-11-04 12:52:54 -0500

    remove missing tables for now

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -11,8 +11,8 @@ import { initTRPC, TRPCError } from '@trpc/server';
 import superjson from 'superjson';
 import { ZodError } from 'zod';
 
-import { auth } from '~/server/auth';
-import { db } from '~/server/db';
+import { auth } from '@/server/auth';
+import { db } from '@/server/db';
 
 /**
  * 1. CONTEXT
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,8 +2,8 @@ import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { type DefaultSession, type NextAuthConfig } from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
 
-import { db } from '~/server/db';
-import { accounts, sessions, users, verificationTokens } from '~/server/db/schema';
+import { db } from '@/server/db';
+import { accounts, users } from '@/server/db/schema';
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -47,8 +47,6 @@ export const authConfig = {
   adapter: DrizzleAdapter(db, {
     usersTable: users,
     accountsTable: accounts,
-    sessionsTable: sessions,
-    verificationTokensTable: verificationTokens,
   }),
   callbacks: {
     session: ({ session, user }) => ({
